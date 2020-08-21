9fe2b2c851439551fd8c91e039e3b64f1ff2ed0c
Jules de Smit <julesdesmit@gmail.com>
2020-08-21 16:10:30 +0200

    Update provisioner prestaking contract according to new design document

--- full contract/PrestakingProvisioner.sol
+++ full contract/PrestakingProvisioner.sol
@@ -542,30 +542,37 @@ contract PrestakingProvisioner is Ownable {
         uint256 amount;
         uint256 accumulatedReward;
         uint    cooldownTime;
-        bool    active;
         uint256 pendingReward;
+        uint256 dailyReward;
+        uint    lastUpdated;
     }
     
     mapping(address => Staker) public stakersMap;
-    address[] public allStakers;
-    uint256 public minimumStake;
-    uint256 public maximumStake;
     uint256 public dailyRewardPercentage;
-    
-    uint private lastUpdated;
-    
+    uint256 public stakersAmount;
+
+    uint public deactivationTime;
+        
     modifier onlyStaker() {
         Staker storage staker = stakersMap[msg.sender];
-        require(staker.startTime.add(1 days) <= block.timestamp && staker.startTime != 0, "No stake is active for sender address");
+        uint startTime = staker.startTime;
+        require(startTime.add(1 days) <= block.timestamp && startTime != 0, "No stake is active for sender address");
+        _;
+    }
+
+    modifier onlyActive() {
+        require(deactivationTime == 0);
+        _;
+    }
+
+    modifier onlyInactive() {
+        require(deactivationTime != 0);
         _;
     }
     
-    constructor(IERC20 token, uint256 min, uint256 max, uint256 rewardPercentage, uint timestamp) public {
+    constructor(IERC20 token, uint256 rewardPercentage) public {
         _token = token;
-        minimumStake = min;
-        maximumStake = max;
         dailyRewardPercentage = rewardPercentage;
-        lastUpdated = timestamp;
     }
     
     /**
@@ -574,78 +581,78 @@ contract PrestakingProvisioner is Ownable {
     receive() external payable {
         revert();
     }
-    
+
     /**
-     * @notice Update the minimum stake amount.
-     * Can only be called by the contract owner.
-     * 
-     * @param amount The amount to set the minimum stake to.
+     * @notice Deactivate the contract. Only to be used once the campaign
+     * comes to an end.
+     *
+     * NOTE that this sets the contract to inactive indefinitely, and will
+     * not be usable from this point onwards.
      */
-    function updateMinimumStake(uint256 amount) external onlyOwner {
-        require(amount <= maximumStake, "Given amount exceeds current maximum stake");
-        minimumStake = amount;
-    }
-    
-    /**
-     * @notice Update the maximum stake amount.
-     * Can only be called by the contract owner.
-     * 
-     * @param amount The amount to set the maximum stake to.
-     */
-    function updateMaximumStake(uint256 amount) external onlyOwner {
-        require(amount >= minimumStake, "Given amount is less than current minimum stake");
-        maximumStake = amount;
+    function deactivate() external onlyOwner onlyActive {
+        deactivationTime = block.timestamp;
     }
 
     /**
-     * @notice Update the daily reward percentage.
-     * Can only be called by the contract owner.
-     * 
-     * @param rewardPercentage The amount to set the daily reward percentage to.
-     * Note that the reward percentage is precise to three decimals. E.g. setting it to
-     * `1000` gives you 1% increase per day.
+     * @notice Can be used by the contract owner to return a user's stake back to them,
+     * without need for going through the withdrawal period. This should only really be used
+     * at the end of the campaign, if a user does not manually withdraw their stake.
+     * @dev This function only works on single addresses, in order to avoid potential
+     * deadlocks caused by high gas requirements.
      */
-    function updateDailyRewardPercentage(uint256 rewardPercentage) external onlyOwner {
-        dailyRewardPercentage = rewardPercentage;
+    function returnStake(address _staker) external onlyOwner {
+        Staker storage staker = stakersMap[_staker];
+        require(staker.amount > 0, "This person is not staking");
+        
+        uint comparisonTime = block.timestamp;
+        if (deactivationTime != 0) {
+            comparisonTime = deactivationTime;
+        }
+
+        distributeRewards(staker, comparisonTime);
+
+        // If this user has a pending reward, add it to the accumulated reward before
+        // paying him out.
+        staker.accumulatedReward = staker.accumulatedReward.add(staker.pendingReward);
+        removeUser(staker, _staker);
     }
     
     /**
      * @notice Lock up a given amount of DUSK in the pre-staking contract.
      * @dev A user is required to approve the amount of DUSK prior to calling this function.
      */
-    function stake() external {
+    function stake(uint256 amount) external onlyActive {
         // Ensure this staker does not exist yet.
         Staker storage staker = stakersMap[msg.sender];
         require(staker.amount == 0, "Address already known");
-        
-        // Check that the staker has approved the appropriate amount of DUSK to this contract.
-        uint256 balance = _token.allowance(msg.sender, address(this));
-        require(balance != 0, "No tokens have been approved for this contract");
-        require(balance >= minimumStake, "Insufficient tokens approved for this contract");
 
-        if (balance > maximumStake) {
-            balance = maximumStake;
+        if (amount > 1000000 ether || amount < 10000 ether) {
+            revert("Amount to stake is out of bounds");
         }
         
         // Set information for this staker.
-        allStakers.push(msg.sender);
-        staker.amount = balance;
-        staker.startTime = block.timestamp;
+        uint blockTimestamp = block.timestamp;
+        staker.amount = amount;
+        staker.startTime = blockTimestamp;
+        staker.lastUpdated = blockTimestamp;
+        staker.dailyReward = amount.mul(dailyRewardPercentage.add(100000)).div(100000).sub(amount);
+        stakersAmount++;
         
         // Transfer the DUSK to this contract.
-        _token.safeTransferFrom(msg.sender, address(this), balance);
+        _token.safeTransferFrom(msg.sender, address(this), amount);
     }
     
     /**
      * @notice Start the cooldown period for withdrawing a reward.
      */
-    function startWithdrawReward() external onlyStaker {
+    function startWithdrawReward() external onlyStaker onlyActive {
         Staker storage staker = stakersMap[msg.sender];
+        uint blockTimestamp = block.timestamp;
         require(staker.cooldownTime == 0, "A withdrawal call has already been triggered");
         require(staker.endTime == 0, "Stake already withdrawn");
-        distributeRewards();
+        distributeRewards(staker, blockTimestamp);
         
-        staker.cooldownTime = block.timestamp;
+        staker.cooldownTime = blockTimestamp;
         staker.pendingReward = staker.accumulatedReward;
         staker.accumulatedReward = 0;
     }
@@ -655,9 +662,10 @@ contract PrestakingProvisioner is Ownable {
      */
     function withdrawReward() external onlyStaker {
         Staker storage staker = stakersMap[msg.sender];
-        require(staker.cooldownTime != 0, "The withdrawal cooldown has not been triggered");
+        uint cooldownTime = staker.cooldownTime;
+        require(cooldownTime != 0, "The withdrawal cooldown has not been triggered");
 
-        if (block.timestamp.sub(staker.cooldownTime) >= 7 days) {
+        if (block.timestamp.sub(cooldownTime) >= 7 days) {
             uint256 reward = staker.pendingReward;
             staker.cooldownTime = 0;
             staker.pendingReward = 0;
@@ -668,16 +676,38 @@ contract PrestakingProvisioner is Ownable {
     /**
      * @notice Start the cooldown period for withdrawing the stake.
      */
-    function startWithdrawStake() external onlyStaker {
+    function startWithdrawStake() external onlyStaker onlyActive {
         Staker storage staker = stakersMap[msg.sender];
-        require(staker.startTime.add(30 days) <= block.timestamp, "Stakes can only be withdrawn 30 days after initial lock up");
+        uint blockTimestamp = block.timestamp;
+        require(staker.startTime.add(30 days) <= blockTimestamp, "Stakes can only be withdrawn 30 days after initial lock up");
         require(staker.endTime == 0, "Stake already withdrawn");
         require(staker.cooldownTime == 0, "A withdrawal call has been triggered - please wait for it to complete before withdrawing your stake");
         
         // We distribute the rewards first, so that the withdrawing staker
         // receives all of their allocated rewards, before setting an `endTime`.
-        distributeRewards();
-        staker.endTime = block.timestamp;
+        distributeRewards(staker, blockTimestamp);
+        staker.endTime = blockTimestamp;
+    }
+    
+    /**
+     * @notice Start the cooldown period for withdrawing the stake.
+     * This function can only be called once the contract is deactivated.
+     * @dev This function is nearly identical to `startWithdrawStake`,
+     * but it was included in order to prevent adding a `SLOAD` call
+     * to `distributeRewards`, making contract usage a bit cheaper during
+     * the campaign.
+     */
+    function startWithdrawStakeAfterDeactivation() external onlyStaker onlyInactive {
+        Staker storage staker = stakersMap[msg.sender];
+        uint blockTimestamp = block.timestamp;
+        require(staker.startTime.add(30 days) <= blockTimestamp, "Stakes can only be withdrawn 30 days after initial lock up");
+        require(staker.endTime == 0, "Stake already withdrawn");
+        require(staker.cooldownTime == 0, "A withdrawal call has been triggered - please wait for it to complete before withdrawing your stake");
+        
+        // We distribute the rewards first, so that the withdrawing staker
+        // receives all of their allocated rewards, before setting an `endTime`.
+        distributeRewards(staker, deactivationTime);
+        staker.endTime = blockTimestamp;
     }
     
     /**
@@ -685,65 +715,41 @@ contract PrestakingProvisioner is Ownable {
      */
     function withdrawStake() external onlyStaker {
         Staker storage staker = stakersMap[msg.sender];
-        require(staker.endTime != 0, "Stake withdrawal call was not yet initiated");
+        uint endTime = staker.endTime;
+        require(endTime != 0, "Stake withdrawal call was not yet initiated");
         
-        if (block.timestamp.sub(staker.endTime) >= 7 days) {
-            uint256 balance = staker.amount.add(staker.accumulatedReward);
-            delete stakersMap[msg.sender];
-            
-            // Delete staker from the array.
-            for (uint i = 0; i < allStakers.length; i++) {
-                if (allStakers[i] == msg.sender) {
-                    allStakers[i] = allStakers[allStakers.length-1];
-                    delete allStakers[allStakers.length-1];
-                }
-            }
-
-            _token.safeTransfer(msg.sender, balance);
+        if (block.timestamp.sub(endTime) >= 7 days) {
+            removeUser(staker, msg.sender);
         }
     }
     
     /**
-     * @notice Update the reward allocation, step-by-step.
-     * @dev This function can update the staker's active status, and the staking pool size.
+     * @notice Update the reward allocation for a given staker.
+     * @param staker The staker to update the reward allocation for.
      */
-    function distributeRewards() internal {
-        while ((block.timestamp.sub(lastUpdated)) > 1 days) {
-            lastUpdated = lastUpdated.add(1 days);
-
-            // Update the staking pool for this day
-            updateStakingPool();
-            
-            // Allocate rewards for this day.
-            for (uint i = 0; i < allStakers.length; i++) {
-                Staker storage staker = stakersMap[allStakers[i]];
-                
-                // Stakers can only start receiving rewards after 1 day of lockup.
-                // If the staker has called to withdraw their stake, don't allocate any more rewards to them.
-                if (!staker.active || staker.endTime != 0) {
-                    continue;
-                }
-                
-                // Calculate percentage of reward to be received, and allocate it.
-                // Reward is calculated down to a precision of three decimals.
-                uint256 reward = staker.amount.mul(dailyRewardPercentage.add(100000)).div(100000).sub(staker.amount);
-                staker.accumulatedReward = staker.accumulatedReward.add(reward);
-            }
+    function distributeRewards(Staker storage staker, uint comparisonTime) internal {
+        uint numDays = comparisonTime.sub(staker.lastUpdated).div(1 days);
+        if (numDays == 0) {
+            return;
         }
+        
+        uint256 reward = staker.dailyReward.mul(numDays);
+        staker.accumulatedReward = staker.accumulatedReward.add(reward);
+        staker.lastUpdated = staker.lastUpdated.add(numDays.mul(1 days));
     }
 
     /**
-     * @notice Update the staking pool, if any new entrants are found which have passed
-     * the initial waiting period.
+     * @notice Remove a user from the staking pool. This ensures proper deletion from
+     * the stakers map and the stakers array, and ensures that all DUSK is returned to
+     * the rightful owner.
+     * @param staker The information of the staker in question
+     * @param sender The address of the staker in question
      */
-    function updateStakingPool() internal {
-        for (uint i = 0; i < allStakers.length; i++) {
-            Staker storage staker = stakersMap[allStakers[i]];
-
-            // If this staker has just become active, update the staking pool size.
-            if (!staker.active && lastUpdated.sub(staker.startTime) >= 1 days) {
-                staker.active = true;
-            }
-        }
+    function removeUser(Staker storage staker, address sender) internal {
+        uint256 balance = staker.amount.add(staker.accumulatedReward);
+        delete stakersMap[sender];
+        stakersAmount--;
+        
+        _token.safeTransfer(sender, balance);
     }
 }
